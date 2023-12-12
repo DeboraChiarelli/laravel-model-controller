bcdeefb3fd7648324dc07d3f72ce08ae4b25d643
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-12-12 15:36:58 +0100

    Add - index.blade.php

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        // Recupera tutti i film dal database utilizzando il model Movie
+        $movies = Movie::all();
+
+        // Passa i film alla view
+        return view('guest.index', compact('movies'));
+    }
 }
